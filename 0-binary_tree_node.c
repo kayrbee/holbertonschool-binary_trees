7e5a9d351b045d660f13721b569772be7d04fff9
Katherine Beringer <github@katbee.au>
2025-08-04 03:47:43 +0000

    Set up task 0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+/**
+ * binary_tree_node - create a new node
+ * @parent: pointer to parent node
+ * @value: value assignment for new node
+ * Return: pointer to new node, or NULL on failure
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+}
+
