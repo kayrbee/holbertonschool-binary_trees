f1cfcabf2695a92b64d1501b1f4609d7b196b6f7
Katherine Beringer <github@katbee.au>
2025-08-04 06:21:43 +0000

    Fix/handle top of tree correctly

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -7,8 +7,13 @@
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node != NULL && (node->left != NULL && node->right != NULL))
-		return (1);
+	if (node != NULL)
+	{
+		if (node->parent == NULL)
+			return (1);
+		if (node->left != NULL && node->right != NULL)
+			return (1);
+	}
 	return (0);
 
 }
