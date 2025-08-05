2dc9b7ad02e2134ee2a9560038a67f36dd6578c3
Katherine Beringer <github@katbee.au>
2025-08-05 02:40:36 +0000

    Fix betty

--- binary_trees.h
+++ binary_trees.h
@@ -34,7 +34,5 @@ void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
-// void print_num(int n);
-// void (*func)(int);
 
 #endif
