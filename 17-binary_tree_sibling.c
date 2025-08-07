490f363e7e1c84532fb6e6e8820f46b51f3a3705
Katherine Beringer <github@katbee.au>
2025-08-07 09:01:22 +0000

    Fix/ same bug in ex 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,10 +8,11 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	binary_tree_t *parent = node->parent;
+	binary_tree_t *parent;
 
-	if (node == NULL || parent == NULL)
+	if (node == NULL || node->parent == NULL)
 	       return (NULL);
+	parent = node->parent;
 	if (node == parent->left && parent->right != NULL)
 		return (parent->right);
 	if (node == parent->right && parent->left != NULL)
